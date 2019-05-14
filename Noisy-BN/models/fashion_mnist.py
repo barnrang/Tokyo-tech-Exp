4813bfd4d74c7b9c8db98491dcb3ba1cfb280d03
barnrang <barnrang@gmail.com>
2019-05-14 16:51:16 +0900

    add better noisy and schedule

--- Noisy-BN/models/fashion_mnist.py
+++ Noisy-BN/models/fashion_mnist.py
@@ -7,24 +7,24 @@ sys.path.append(HOME_PATH)
 import tensorflow as tf
 from tensorflow import keras
 
-from layers.noisy_bn import NoisyBatchNormalization
+from layers.noisy_bn import BetterNoisyBatchNormalization
 
 def get_model(alpha=0., p=0.25, clear_session=True):
     feature_layers = [
         keras.layers.Conv2D(32,(2,2),padding='valid',activation='relu',input_shape=(28,28,1)),
-        NoisyBatchNormalization(alpha,p),
+        BetterNoisyBatchNormalization(alpha,p),
         keras.layers.MaxPooling2D(),
         keras.layers.Conv2D(128,(2,2),padding='valid',activation='relu'),
-        NoisyBatchNormalization(alpha,p),
+        BetterNoisyBatchNormalization(alpha,p),
         keras.layers.MaxPooling2D(),
         keras.layers.Flatten()
     ]
 
     classify_layer = [
         keras.layers.Dense(128,activation='relu'),
-        NoisyBatchNormalization(alpha,p),
+        BetterNoisyBatchNormalization(alpha,p),
         keras.layers.Dense(64,activation='relu'),
-        NoisyBatchNormalization(alpha,p),
+        BetterNoisyBatchNormalization(alpha,p),
         keras.layers.Dense(10,activation='softmax')
     ]
 
